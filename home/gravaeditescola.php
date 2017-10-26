d4f5a85a6066ed28d2b2a386b7ef763655208cd1
TiagoTiburcio <tiburciocosta@gmail.com>
2017-10-26 09:14:51 -0300

    corrigindo textos marcou não

--- home/gravaeditescola.php
+++ home/gravaeditescola.php
@@ -32,58 +32,63 @@
     $escola->atuEscola($codigo, $usuario->getUsuario(), $data, $adminE, $diarioE, $lteE, $wifiE, $observacaoEscola);
     
     //Administrativo    
-    $observacaoAdmin = $_POST ["observacaoAdmin"];
+    
     if ($adminE == "0"){
         $admComputadores = "0";
         $admImpressoras = "0";
         $admEstabilizadores = "0";
         $admScanners = "0";
+        $observacaoAdmin = " Marcou não Possui Setor Administrativo ";
     } else if ($adminE == "1") {
         $admComputadores = $_POST ["admComputadores"];
         $admImpressoras = $_POST ["admImpressora"];
         $admEstabilizadores	= $_POST ["admEstabilizadores"];
         $admScanners = $_POST ["admScanners"];
+        $observacaoAdmin = $_POST ["observacaoAdmin"];
     }    
     
     $administrativo->atuAdm($codigo, $usuario->getUsuario(), $data, $admComputadores, $admImpressoras, $admEstabilizadores, $admScanners, $observacaoAdmin);
     
     //Laboratório    
-    $observacaoLte = $_POST ["observacaoLte"];
-    
     if ($lteE == "0") {
         $lteComputadores = "0";
         $lteImpressoras = "0";
         $lteEstabilizadores = "0";
         $lteCapacidade = "0";
         $ultPregao = "0";
+        $observacaoLte = " Marcou não Possui LTE ";
     } else if ($lteE == "1") {
         $lteComputadores = $_POST ["lteComputadores"];
         $lteImpressoras = $_POST ["lteImpressoras"];
         $lteEstabilizadores = $_POST ["lteEstabilizadores"];
         $lteCapacidade = $_POST ["lteCapacidade"];
         $ultPregao = $_POST ["ultPregao"];
+        $observacaoLte = $_POST ["observacaoLte"];
     }
 
     $laboratorio->atuLab($codigo, $usuario->getUsuario(), $data, $lteComputadores, $lteImpressoras, $lteEstabilizadores, $lteCapacidade, $ultPregao, $observacaoLte);
     
     //Wifi 
-    $observacaoWifi = $_POST ["observacaoWifi"];    
+    
     if ($wifiE == "0") {
         $wifiApRouter = "0";
         $wifiAp = "0";
+        $observacaoWifi = " Marcou não Possui Redes Wifi ";
     } else if ($wifiE = "1") {
         $wifiApRouter = $_POST ["wifiApRouter"];
         $wifiAp = $_POST ["wifiAp"];
+        $observacaoWifi = $_POST ["observacaoWifi"];
     }
     
-    //Tablet
-    $diarioTablet = $_POST ["diarioTablet"];
-    $observacaoDiario = $_POST ["observacaoDiario"];
+    $wifi->atuWifi($codigo, $usuario->getUsuario(), $data, $wifiAp, $wifiApRouter, $observacaoWifi);
     
+    //Tablet
     if ($diarioE == "0") {
         $diarioTablet = "0";
+        $observacaoDiario = " Marcou não Possui Diario Eletrônico ";
     } else if ($diarioE == "1") {
         $diarioTablet = $_POST ["diarioTablet"];
+        $observacaoDiario = $_POST ["observacaoDiario"];
     }
     
     $diario->atuDiario($codigo, $usuario->getUsuario(), $data, $diarioTablet, $observacaoDiario);
